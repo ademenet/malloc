9c75d6d0f4820c784ea98b6e7f335faa32f5fc36
Alain DEMENET <ademenet@e2r6p18.42.fr>
2017-10-04 16:54:04 +0200

    Before test to solve bug with test_tiny_loop param 32 and max 8191 and 8192

--- allocations.c
+++ allocations.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 11:13:50 by ademenet          #+#    #+#             */
-/*   Updated: 2017/10/04 12:10:43 by ademenet         ###   ########.fr       */
+/*   Updated: 2017/10/04 16:53:02 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,8 @@ static void		*allocate_large(size_t size)
 }
 
 /*
-** 
+** Finds the first fit block. Could be optimized with best fit: find the block
+** that fits the best to our demand.
 */
 
 /* TODO mettre la fonction de coalescence dans le parcours de la liste aussi */
@@ -67,19 +68,9 @@ static void		*find_fit(t_block *list, size_t true_size)
 }
 
 /*
-static t_block	*place(t_block *tobe_placed, size_t true_size)
-{
-	tobe_placed->size = 
-	return ;
-}
+** Extends the heap by calling one more time mmap() function. This new zone is
+** linked to the old one in the same linked list.
 */
-
-/*
-**
-*/
-
-/* PBLM comment faire pour relier la nouvelle zone ? il me faut le ptr sur le dernier des headers
-REPBLM mon find_fit me renvoit NULL... il faut quil me renvoit le dernier pointeur et pas NULL */
 
 static t_block	*extend_heap(t_block *list, size_t size, size_t true_size,
 	t_type type)
@@ -90,27 +81,45 @@ static t_block	*extend_heap(t_block *list, size_t size, size_t true_size,
 
 	new_block = (type == TINY) ? mmap(0, TINY_ZONE, PROT_SET, MAP_SET, -1, 0) :\
 		mmap(0, SMALL_ZONE, PROT_SET, MAP_SET, -1, 0);
+	debug("new_block = %p", new_block);
+	debug("true_size = %zu", true_size);
+	getchar();
 	new_block->free = 0;
 	new_block->size = size;
 	prev_block = list;
 	while (prev_block->next)
 		prev_block = prev_block->next;
+	// debug("prev_block = %p", prev_block);
+	// debug("prev_block->next = %p", prev_block->next);
 	new_block->prev = prev_block;
 	prev_block->next = new_block;
+	// debug("prev_block->next = %p", prev_block->next);
 	next_block = (void*)new_block + true_size;
+	// debug("next_block = %p", next_block);
 	new_block->next = next_block;
+	// debug("new_block->next = %p", new_block->next);
 	if (type == TINY)
-		next_block->size = TINY_ZONE - HEADER_SIZE;
+		next_block->size = TINY_ZONE - true_size;
 	else if (type == SMALL)
-		next_block->size = SMALL_ZONE - HEADER_SIZE;
+		next_block->size = SMALL_ZONE - true_size;
+	// debug("next_block->size = %zu", next_block->size);
 	next_block->free = 1;
 	next_block->prev = new_block;
+	// debug("next_block->prev = %p", next_block->prev);
 	next_block->next = NULL;
-	return (list);
+	// getchar();
+	return (new_block);
 }
 
 /*
-** Manages tiny and small allocations.
+** Manages tiny and small allocations by placing headers and returning the
+** available space afterwards.
+*/
+
+/* TODO Segfault du a un decalage dans la seconde zone : 
+mon algo croit quil y a plus de places restantes quil ny en a pour de vrai
+peut etre solution : dans linitalisation de la seconde page, je cree un decalage
+des le debut.
 */
 
 static t_block	*allocate(size_t size, t_block *list, t_type type)
@@ -123,11 +132,17 @@ static t_block	*allocate(size_t size, t_block *list, t_type type)
 		ALIGN((size + HEADER_SIZE), SMALL_RES);
 	if ((new_block = find_fit(list, true_size)) != NULL)
 	{
+		debug("new_block in find_fit : %p", new_block);
 		if (((new_block->size + HEADER_SIZE) - true_size) > 0)
 		{
+			debug("(%zu + %lu) - %zu = %d", new_block->size,HEADER_SIZE,true_size,(new_block->size + HEADER_SIZE) - true_size);
 			next_block = (void*)new_block + true_size;
-			next_block->size = new_block->size - true_size;
+			debug("next_block = %p", next_block);
+			debug("next_block->free = %d", next_block->free); 
 			next_block->free = 1;
+			debug("new_block->size = %zu", new_block->size);
+			debug("next_block->size = %zu", next_block->size);
+			next_block->size = new_block->size - true_size;
 			next_block->prev = new_block;
 			next_block->next = new_block->next;
 			new_block->next = next_block;
@@ -137,6 +152,7 @@ static t_block	*allocate(size_t size, t_block *list, t_type type)
 	}
 	else
 		new_block = extend_heap(list, size, true_size, type);
+	debug("new_block : %p", new_block);
 	return (new_block);
 }
 
@@ -171,7 +187,7 @@ static void		init_malloc(void)
 
 /*
 ** Main malloc function not thread safe (NTS).
-** Returns a point to the payload (rounded to resolution of type) of a memory
+** Returns a pointer to the payload (rounded to resolution of type) of a memory
 ** block.
 */
 
@@ -186,5 +202,6 @@ void			*malloc_nts(size_t size)
 		new = allocate(size, g_bin.small, SMALL);
 	else
 		new = allocate_large(size);
+	debug("new = %p", new);
 	return ((void*)new + HEADER_SIZE);
 }
