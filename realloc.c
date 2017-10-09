199c1df8e88546186ca84ad08d2385d7351c78a8
Alain DEMENET <ademenet@e2r5p17.42.fr>
2017-10-09 14:03:04 +0200

    First draft of realloc

--- realloc.c
+++ realloc.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/02 14:00:36 by ademenet          #+#    #+#             */
-/*   Updated: 2017/10/02 14:08:45 by ademenet         ###   ########.fr       */
+/*   Updated: 2017/10/09 14:00:26 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,20 @@
 
 void			*realloc_nts(void *ptr, size_t size)
 {
+	t_block		*tmp;
+
+	tmp = (void *)ptr - HEADER_SIZE;
+	if (tmp && tmp->next)
+	{
+		if (tmp->next->free == 1 && (tmp->size + tmp->next->free + HEADER_SIZE
+			>= size))
+		{
+			tmp->next = tmp->next->next;
+			tmp->next->prev = tmp;
+		}
+		else
+			tmp = malloc_nts(size);
+		return (tmp)
+	}
 	return(NULL);
 }
