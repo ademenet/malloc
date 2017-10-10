64c422cd4c5442ab45efd204079abeb7c828789e
Alain DEMENET <ademenet@e2r5p17.42.fr>
2017-10-10 17:29:42 +0200

    Better display of blocks in a list

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/02 16:53:35 by ademenet          #+#    #+#             */
-/*   Updated: 2017/10/09 18:10:31 by ademenet         ###   ########.fr       */
+/*   Updated: 2017/10/10 16:14:29 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,28 @@ t_type			which_type(size_t size)
 
 void			display_list_of_blocks(t_block *list)
 {
-    t_block		*tmp;
+	t_block		*tmp;
+	t_type		type;
+	size_t			true_size;
 
 	tmp = list;
+	if (list == g_bin.tiny)
+		type = TINY;
+	else if (list == g_bin.small)
+		type = SMALL;
+	else
+		type = LARGE;
 	while(tmp)
 	{
-		fprintf(stderr, "list->size:\t%zu\nlist->next:\t%p\nlist->free:\t%d\n",\
-				  list->size, list->next, list->free);
-		fprintf(stderr, "---\n");
+		fprintf(stderr, "[ ");
+		if (tmp->free == 1)
+			fprintf(stderr, "%s%12p <| prev | cur %p | size %10zu | free %d | next |> %-12p%s",\
+					GRN, tmp->prev, (void*)tmp, tmp->size, tmp->free, tmp->next, END);
+		else if (tmp->free == 0)
+			fprintf(stderr, "%s%12p <| prev | cur %p | size %10zu | free %d | next |> %-12p%s",\
+				RED, tmp->prev, (void*)tmp, tmp->size, tmp->free, tmp->next, END);
+		true_size = (type == TINY) ? ALIGN((tmp->size + HEADER_SIZE), TINY_RES) : ALIGN((tmp->size + HEADER_SIZE), SMALL_RES);
+		fprintf(stderr, " ] %-10zu\n", true_size);
 		tmp = tmp->next;
 	}
 }
