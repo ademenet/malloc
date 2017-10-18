cdd4f5531cfdc180819381dd9efa38cfa6a83f9a
Alain DEMENET <ademenet@e3r7p20.42.fr>
2017-10-18 18:16:56 +0200

    Solved

--- test/compile.sh
+++ test/compile.sh
@@ -4,4 +4,4 @@ gcc -o test1 test/test1.c
 gcc -o test2 test/test2.c
 gcc -o test3 test/test3.c
 gcc -o test4 test/test4.c
-gcc -o test5 test5.c -L. -lft_malloc
+gcc -o test5 test/test5.c -L. -lft_malloc
